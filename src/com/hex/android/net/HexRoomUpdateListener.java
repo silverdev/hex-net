cf357cb38abc3d701020a3b70b4c6d3b394d067f
Will <xlythe@gmail.com>
2013-06-15 23:45:43 -0400

    remove constants

--- src/com/hex/android/net/HexRoomUpdateListener.java
+++ src/com/hex/android/net/HexRoomUpdateListener.java
@@ -6,35 +6,34 @@ import android.content.Intent;
 import com.google.android.gms.games.GamesClient;
 import com.google.android.gms.games.multiplayer.realtime.Room;
 import com.google.android.gms.games.multiplayer.realtime.RoomUpdateListener;
-import com.sam.hex.MainActivity;
 
 public class HexRoomUpdateListener implements RoomUpdateListener {
+    private static final int RC_WAITING_ROOM = 1025;
     private Activity mMainActivity;
     private String mRoomId;
     private GameManager gameManager;
 
-
     public HexRoomUpdateListener(Activity main, GameManager gameManager) {
-    	mMainActivity = main;
-    	this.gameManager = gameManager;
-	}
+        mMainActivity = main;
+        this.gameManager = gameManager;
+    }
 
-	@Override
+    @Override
     public void onJoinedRoom(int statusCode, Room room) {
         if(statusCode != GamesClient.STATUS_OK) {
             // display error
             return;
         }
-        //If I joined then I am not the host
+        // If I joined then I am not the host
         this.gameManager.host = false;
-        
-        //tell the game gamager the room id
+
+        // tell the game gamager the room id
         mRoomId = room.getRoomId();
         this.gameManager.mRoomId = mRoomId;
         // get waiting room intent
-		System.out.println("joining");
+        System.out.println("joining");
         Intent i = gameManager.mHelper.getGamesClient().getRealTimeWaitingRoomIntent(room, 1);
-        mMainActivity.startActivityForResult(i, MainActivity.RC_WAITING_ROOM);
+        mMainActivity.startActivityForResult(i, RC_WAITING_ROOM);
         System.out.println("room is gone");
     }
 
@@ -46,9 +45,9 @@ public class HexRoomUpdateListener implements RoomUpdateListener {
 
     @Override
     public void onRoomConnected(int status, Room room) {
-        //set a list of all the players
-    	gameManager.connectedPlayers = room.getParticipantIds();
-    	gameManager.makeGame(); 
+        // set a list of all the players
+        gameManager.connectedPlayers = room.getParticipantIds();
+        gameManager.makeGame();
 
     }
 
@@ -61,14 +60,14 @@ public class HexRoomUpdateListener implements RoomUpdateListener {
         // If i made the room I am host
         System.out.print("I am hosting");
         this.gameManager.host = true;
-        
+
         mRoomId = room.getRoomId();
         this.gameManager.mRoomId = mRoomId;
         // get waiting room intent
         Intent i = gameManager.mHelper.getGamesClient().getRealTimeWaitingRoomIntent(room, Integer.MAX_VALUE);
-        mMainActivity.startActivityForResult(i, MainActivity.RC_WAITING_ROOM);
+        mMainActivity.startActivityForResult(i, RC_WAITING_ROOM);
         System.out.print("game is started!");
-      
+
     }
 
 }
